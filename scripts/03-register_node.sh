bb55463d3846ebb4d590e3e7852f30fabeefc7ea
fly4us <fly4us@gmail.com>
2019-02-14 17:15:19 +0800

    Update 03-register_node.sh

--- scripts/03-register_node.sh
+++ scripts/03-register_node.sh
@@ -8,13 +8,13 @@ installed=$(psql -qAt -h "$PGHOST" -U "$REPMGR_USER" "$REPMGR_DB" -c "SELECT 1 F
  
 if [ "${installed}" != "1" ]; then
     echo '~~ 03: registering as primary' >&2
-    env –u PGPASSWORD repmgr -f ${PGDATA}/repmgr.conf primary register
+    env -u PGPASSWORD repmgr -f ${PGDATA}/repmgr.conf primary register
     exit 0
 fi
 
 if [ -n "$WITNESS" ]; then
 	echo '~~ 03: registering as witness server' >&2
-	env –u PGPASSWORD repmgr -f ${PGDATA}/repmgr.conf -h "$PRIMARY_NODE" -U "$REPMGR_USER" -d "$REPMGR_DB" witness register
+	env -u PGPASSWORD repmgr -f ${PGDATA}/repmgr.conf -h "$PRIMARY_NODE" -U "$REPMGR_USER" -d "$REPMGR_DB" witness register
 	exit 0
 fi
  
@@ -25,9 +25,9 @@ if [ "${is_reg}" != "1" ] && [ ${my_node} -gt 1 ]; then
     echo '~~ 03: registering as standby' >&2
     pg_ctl -D "$PGDATA" stop -m fast
     rm -Rf "$PGDATA"/*
-    env –u PGPASSWORD repmgr -f ${PGDATA}/repmgr.conf -h "$PRIMARY_NODE" -U "$REPMGR_USER" -d "$REPMGR_DB" standby clone --fast-checkpoint
+    env -u PGPASSWORD repmgr -f ${PGDATA}/repmgr.conf -h "$PRIMARY_NODE" -U "$REPMGR_USER" -d "$REPMGR_DB" standby clone --fast-checkpoint
     cp /etc/postgresql/11/main/postgresql.conf ${PGDATA}/
     pg_ctl -D "$PGDATA" start &
     sleep 1
-    env –u PGPASSWORD repmgr -f ${PGDATA}/repmgr.conf -h "$PRIMARY_NODE" -U "$REPMGR_USER" -d "$REPMGR_DB" standby register    
+    env -u PGPASSWORD repmgr -f ${PGDATA}/repmgr.conf -h "$PRIMARY_NODE" -U "$REPMGR_USER" -d "$REPMGR_DB" standby register    
 fi
