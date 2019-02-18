ff24dd4ec093910dc56df751a47a929580d04cd3
fly4us <fly4us@gmail.com>
2019-02-18 13:00:17 +0800

    Update 04-repmgrd.sh

--- scripts/04-repmgrd.sh
+++ scripts/04-repmgrd.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
  
-env -u PGPASSWORD repmgrd -f /etc/postgresql/11/main/repmgr.conf -v
+env -u PGPASSWORD PGPASSFILE=/etc/postgresql/11/main/.pgpass repmgrd -f /etc/postgresql/11/main/repmgr.conf -v
